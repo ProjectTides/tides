ae916f95f70bf7db1b460b8c2a63695b649ddb0f
satendra-sr <satendra@coloredcow.com>
2020-05-11 03:03:28 +0530

    Docker pg db init

--- build_scripts/entrypoint.sh
+++ build_scripts/entrypoint.sh
@@ -2,13 +2,13 @@
 # docker entrypoint script.
 
 # assign a default for the database_user
-DB_USER=${DATABASE_USER:-postgres}
+DB_USER=${DATABASE_DB:-postgres}
+DB_HOST=${DATABASE_HOST:-db}
 
 # wait until Postgres is ready
-while ! pg_isready -q -h db -p 5432 -U postgres
+while ! pg_isready -q -h DB_HOST -p 5432 -U DB_USER
 do
-  echo "$(date) - waiting for database to start"
-  echo DATABASE_USER
+  echo "$(date) - waiting ${DATABASE_DB} ${DATABASE_HOST} for database to start"
   sleep 2
 done
 

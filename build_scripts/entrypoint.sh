0e3dc4650a47ad1688151d6532148eedad22830c
rudresh <rudresh.shrotriya@gmail.com>
2020-05-16 19:48:07 +0530

    updated aws docker configs

--- build_scripts/entrypoint.sh
+++ build_scripts/entrypoint.sh
@@ -2,15 +2,15 @@
 # docker entrypoint script.
 
 # assign a default for the database_user / role
-DB_ROLE=${DATABASE_ROLE:-postgres}
-DB_HOST=${DATABASE_HOST:-db}
+# DB_ROLE=${DATABASE_ROLE:-postgres}
+# DB_HOST=${DATABASE_HOST:-db}
 
-# wait until Postgres is ready
-while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_ROLE
-do
-  echo "$(date) - waiting for database to start"
-  sleep 2
-done
+# # wait until Postgres is ready
+# while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_ROLE
+# do
+#   echo "$(date) - waiting for database to start"
+#   sleep 2
+# done
 
 bin="/app/bin/project_tides"
 eval "$bin eval \"ProjectTides.Release.migrate\""

db6bb11095d303a08971ed5f60a8197f6099f7c0
satendra-sr <satendra@coloredcow.com>
2020-05-25 22:23:12 +0530

    Reverted config changes

--- config/releases.exs
+++ config/releases.exs
@@ -2,20 +2,20 @@
 # Added few checks for debug.
 # This file will be used for production release only.
 import Config
-db_host = System.fetch_env!("DATABASE_HOST") || 
+db_host = System.get_env("DATABASE_HOST") || 
   raise """
   environment variable DATABASE_HOST is missing.
   """
-db_database = System.fetch_env!("DATABASE_DB") || "postgres" 
-db_username = System.fetch_env!("DATABASE_USER") || "postgres" 
-db_password = System.fetch_env!("DATABASE_PASSWORD") || "postgres"
+db_database = System.get_env("DATABASE_DB") || "postgres" 
+db_username = System.get_env("DATABASE_USER") || "postgres" 
+db_password = System.get_env("DATABASE_PASSWORD") || "postgres"
 db_url = "ecto://#{db_username}:#{db_password}@#{db_host}/#{db_database}"
 
 config :project_tides, ProjectTides.Repo,
   url:  db_url,
-  pool_size: String.to_integer(System.fetch_env!("POOL_SIZE") || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   show_sensitive_data_on_connection_error: true
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE") ||
+secret_key_base = System.get_env("SECRET_KEY_BASE") ||
   raise """
   environment variable SECRET_KEY_BASE is missing.
   You can generate one by calling: mix phx.gen.secret
